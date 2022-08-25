302ce22f6bd854f3a04c7a5b5e734ad351482271
Andras E <rohadtsajt@gmail.com>
2022-08-25 18:51:22 +0100

    skinsandtoggles

---
+++ js/style-switcher.js
@@ -0,0 +1,4 @@
+const styleSwitcherToggler = document.querySelector(".style-toggler");
+styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open")
+})
