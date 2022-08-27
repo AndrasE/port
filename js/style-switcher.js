5b981b903857ed06642343bdf3ce6205bc98729b
Andras E <rohadtsajt@gmail.com>
2022-08-27 13:41:03 +0100

    style switch auto hide on scroll

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,4 +1,13 @@
+/* ------===== toggle style switcher =====------- */
 const styleSwitcherToggler = document.querySelector(".style-toggler");
 styleSwitcherToggler.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open")
 })
+
+/* ------===== hide style switcher on scroll =====------- */
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open")
+    }
+})
+
