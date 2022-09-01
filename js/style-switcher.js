5fc43bb2c67b2382ccb6427046e50db1df42ed67
Andras E <rohadtsajt@gmail.com>
2022-09-01 17:11:18 +0100

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -11,3 +11,18 @@ window.addEventListener("scroll", () => {
     }
 })
 
+/* ------===== theme colors =====------- */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+
+
