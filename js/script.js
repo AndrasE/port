c9b5700ea551b8197168426dc764b567a88bce52
Andras E <rohadtsajt@gmail.com>
2022-09-01 21:18:16 +0100

    dark/light/typed.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ------===== tying animation =====------- */
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","And More"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
