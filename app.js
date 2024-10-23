65d0bf68a0d8353064acff4c1b5a7293b67e48f4
Andras E <rohadtsajt@gmail.com>
2024-10-23 07:45:06 +0100

    netlify-ing project for deployment

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require("express");
+const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
+const router = express.Router();
 const path = require("path");
+const favicon = require('serve-favicon')
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
@@ -8,6 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // Serve static files (CSS, JS, etc.)
 app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, 'public//images', 'favicon.ico')))
 
 // Define route to render your main page
 app.get("/", (req, res) => {
@@ -17,3 +21,6 @@ app.get("/", (req, res) => {
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
