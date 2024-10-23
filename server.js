a4a617634d58039c0cec6a7bb55bf8f768ff1549
AndrasE <rohadtsajt@gmail.com>
2024-10-23 15:45:59 +0100

    rename

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// src/api.js or src/server.js (adjust naming as needed)
+// server.js
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
 const path = require("path");
+const app = express();
 const favicon = require('serve-favicon');
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../views")); // Adjust path as needed
+app.set("views", path.join(__dirname, "views"));
 
 // Serve static files (CSS, JS, etc.)
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(favicon(path.join(__dirname, '../public/images', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 
 // Define route to render your main page
 app.get("/", (req, res) => {
@@ -23,4 +23,5 @@ app.use('/.netlify/functions/api', (req, res) => {
   res.send('Serverless Function is working!');
 });
 
+// Export serverless handler
 module.exports.handler = serverless(app);
