89cfc4fd7d1af09c53faf1bf9da33b73a5bde580
Andras E <rohadtsajt@gmail.com>
2024-10-23 17:29:18 +0100

    x

--- api.js
+++ api.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // Serve static files (CSS, JS, etc.)
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // Define route to render your main page
 app.get("/", (req, res) => {
