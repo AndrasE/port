04d331b88d82d778fa15bc5329047c65ed6b9c21
AndrasE <rohadtsajt@gmail.com>
2024-10-23 15:37:21 +0100

    adjust

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
+// src/api.js or src/server.js (adjust naming as needed)
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const router = express.Router();
 const path = require("path");
-const favicon = require('serve-favicon')
+const favicon = require('serve-favicon');
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views")); // Adjust path as needed
 
 // Serve static files (CSS, JS, etc.)
-app.use(express.static("public"));
-app.use(favicon(path.join(__dirname, 'public//images', 'favicon.ico')))
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(favicon(path.join(__dirname, '../public/images', 'favicon.ico')));
 
 // Define route to render your main page
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Define a path for serverless functions
+app.use('/.netlify/functions/api', (req, res) => {
+  res.send('Serverless Function is working!');
+});
 
-app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
