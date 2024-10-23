065317b1a416db79fd5147141a3ab98103bb6379
Andras E <rohadtsajt@gmail.com>
2024-10-23 17:39:26 +0100

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const path = require("path");
-const favicon = require('serve-favicon');
+
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // Serve static files (CSS, JS, etc.)
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 
 // Define route to render your main page
 app.get("/", (req, res) => {
