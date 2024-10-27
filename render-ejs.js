97c075f1e9308bce45ec9df8848157832fd9e686
AndrasE <rohadtsajt@gmail.com>
2024-10-27 13:59:55 +0000

    finally

--- render-ejs.js
+++ render-ejs.js
@@ -2,25 +2,21 @@ const ejs = require("ejs");
 const fs = require("fs");
 const path = require("path");
 
-// Path to the EJS file you want to render
 const templatePath = path.join(__dirname, "views", "index.ejs");
-
-// Path to save the rendered HTML output
 const outputPath = path.join(__dirname, "public", "index.html");
 
-// Read the EJS template file
 fs.readFile(templatePath, "utf8", (err, template) => {
   if (err) {
     console.error("Error reading EJS template file:", err);
     return;
   }
 
-  // Render EJS into HTML (you can pass any variables if needed)
-  const htmlContent = ejs.render(template, {
-    // Add any variables here if needed
-  });
+  const options = {
+    root: path.join(__dirname, "views"),
+  };
 
-  // Write the rendered HTML to the output file
+  const htmlContent = ejs.render(template, {}, options);
+
   fs.writeFile(outputPath, htmlContent, (err) => {
     if (err) {
       console.error("Error writing rendered HTML file:", err);
